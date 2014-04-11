80b8bb7b0f0d472ef69a85d803aea6e24fae5043
girishvk <girishvk@umich.edu>
2014-04-11 18:42:39 -0400

    Javascript inventory

--- app.js
+++ app.js
@@ -6,13 +6,33 @@ app.prototype = function () {
     var player = {};
     player.cash = 0;
     player.inventory = [];
-
-    // Private functions
-    
+	
+	var keylogger = [];
+	
+	// Keylogger Begin
+	keylogger.Begin = {}
+	keylogger.Begin.price = 100; // 3 Types of Keylogger( Beginner, Medium, Pro)
+	keylogger.Begin.risk = 0.2; // 
+	Keylogger.Begin.hitfactor = 0.4;
+	
+	// Keylogger Medium
+	keylogger.Medium ={};
+	keylogger.Medium.price = 200;
+	keylogger.Medium.risk = 0.4;
+	keylogger.Medium.hitfactor = 0.6;
+	
+	// Keylogger Hard
+	keylogger.Hard ={};
+	keylogger.Hard.price = 400;
+	keylogger.Hard.risk = 0.6;
+	keylogger.Hard.hitfactor = 0.8;
+	
+ // Private functions
+ 
 
     return {
         // Public functions
-        getCash = function () {
+        getCash: function () {
             return player.cash;    
         }
 
