d6bcf656d8f800f0aa34e5faba493a62355381bf
Eugene Goh <vanblaze5@gmail.com>
2014-04-11 18:44:37 -0400

    Added yeoman angularjs framework, removed bootstrap folder

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('honeypotApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
