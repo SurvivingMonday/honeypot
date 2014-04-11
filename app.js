03906dc1265b6c53d702f3b767586eba48de04c9
Eugene Goh <vanblaze5@gmail.com>
2014-04-11 18:08:51 -0400

    Added app.js and .gitignore

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = {};
+
+app.prototype = function () {
+    
+    // Variables here
+    var player = {};
+    player.cash = 0;
+    player.inventory = [];
+
+    // Private functions
+    
+
+    return {
+        // Public functions
+        getCash = function () {
+            return player.cash;    
+        }
+
+
+
+    }
+
+
+}
