25012b49d6be0f6fc95de703c9f8488256d394cd
Eugene Goh <vanblaze5@gmail.com>
2014-04-12 11:32:55 -0400

    forcing conflicts

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,45 +1,6 @@
 'use strict';
 
 angular.module('honeypotApp')
-    .controller('GameCtrl', function ($scope, GameManager, GameMapService) {
-
-    	var selectedIdx = null;
-    	$scope.player = GameManager.getPlayer();
-    	$scope.blackMarket = GameManager.getBlackM();
-
-    	$scope.notifyBuy = GameManager.buy;
-    	$scope.notifySell = GameManager.sell;
-    	
-    	$scope.selectInv = function(idx) {
-    		if (selectedIdx !== null) {
-    			$scope.player.inventory[selectedIdx].selected = false;
-    		}
-    		selectedIdx = idx;
-    		$scope.player.inventory[idx].selected = true;
-    	};
-
-
-    	GameMapService.addClickListeners(function(event) {
-
-        if($scope.player.inventory.length === 0) {
-
-        } else {
-          var temp = selectedIdx;
-          if (Math.random() < 0.8 && temp !== null) {
-            GameMapService.putKeylogger(event.latLng, function(length, marker) {
-              console.log(length);
-              console.log(marker);
-              GameManager.infect(marker, temp, length);
-              console.log(asdfa);
-              $scope.player = GameManager.getPlayer();
-            });
-          } else {
-            // TODO: Google tag here 'Select a keylogger first'
-
-          }
-        }
-    	})
-    });
 .controller('GameCtrl', function ($scope, $timeout, GameManager, GameMapService) {
 
 	var selectedIdx = null;
