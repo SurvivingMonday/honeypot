5eb8b8525d1f86481f69e51bcd5e85ad2f479875
Sofwan Haddad <sysofwan@umich.edu>
2014-04-12 08:16:45 -0400

    added scope variables

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -4,6 +4,7 @@ angular.module('honeypotApp')
     .controller('GameCtrl', function ($scope, GameManager, GameMapService) {
     	$scope.player = GameManager.getPlayer();
     	$scope.blackMarket = GameManager.getBlackM();
+    	console.log($scope.blackMarket);
 
     	$scope.notifyBuy = GameManager.buy;
     	$scope.notifySell = GameManager.sell;
