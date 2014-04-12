b37615493699138d30dbd7e95b7bf18b8b3d5cdd
Eugene Goh <vanblaze5@gmail.com>
2014-04-12 07:50:50 -0400

    Edited gameManager.js

--- app/scripts/game/gameManager.js
+++ app/scripts/game/gameManager.js
@@ -4,129 +4,172 @@ angular.module('honeypotGame', ['ngCookies'])
     .service('GameManager', function($q, $cookieStore) {
 
       // Player
-      this.cash = 200;
-      this.encryptedData = 0;
-      this.dataA = 0;
-      this.dataB = 0;
-      this.dataC = 0;
-      this.inventory = {};
+      var player = {
+        cash: 200,
+        attentionLevel: 0, // 0 - 10
+        ecData: 0,
+        dataA: 0,
+        dataB: 0,
+        dataC: 0,
+        inventory: [],
+        marker: []
+      };
+
+      // Utilities
+      var gametime = 0;
 
       // Player Private functions
 
       // Inventories
-      // 0: Keylogger, 1: Analyzer
-      // ------------ Keyloggers ------------
-      var k1 = {
-        name: 'TT130',
-        type: 0,
-        price: 100,
-        risk: 0.2,
-        hitfactor: 0.1,
-        expiryTime: 0
-      };
 
-      var k2 = {
-        name: 'XH-5',
-        type: 0,
-        price: 150,
-        risk: 0.1,
-        hitfactor: 0.1,
-        expiryTime: 0
-      };
-
-      var k3 = {
-        name: 'TT230',
-        type: 0,
-        price: 200,
-        risk: 0.4,
-        hitfactor: 0.2,
-        expiryTime: 15
-      };
-
-      var k4 = {
-        name: 'XH-7',
-        type: 0,
-        price: 350,
-        risk: 0.4,
-        hitfactor: 0.25,
-        expiryTime: 15
-      };
-
-      var k5 = {
-        name: 'TT330',
-        type: 0,
-        price: 400,
-        risk: 0.6,
-        hitfactor: 0.4,
-        expiryTime: 30
-      };
-
-      // ------------ Analyzers -------------
-      var a1 = {
-        name: 'AZ9900',
-        type: 1,
-        price: 100,
-        dataA: 0.4,
-        dataB: 0.1,
-        dataC: 0.01,
-        duration: 6
-      };
-
-      var a2 = {
-        name: 'AZ8800',
-        type: 1,
-        price: 180,
-        dataA: 0.4,
-        dataB: 0.15,
-        dataC: 0.05,
-        duration: 7
-      };
-
-      var a3 = {
-        name: 'TM2230',
-        type: 1,
-        price: 250,
-        dataA: 0.5,
-        dataB: 0.2,
-        dataC: 0.05,
-        duration: 5
-      };
-
-      var a4 = {
-        name: 'SS8000',
-        type: 1,
-        price: 600,
-        dataA: 0.7,
-        dataB: 0.35,
-        dataC: 0.15,
-        duration: 7
-      };
-
-      var a5 = {
-        name: 'SS9999',
-        type: 1,
-        price: 1500,
-        dataA: 0.85,
-        dataB: 0.4,
-        dataC: 0.2,
-        duration: 8
-      };
 
       // Black Market
       var blackm = {};
-      blackm.onsale = {};
-      blackm.onsale.keylogger = {};
+      blackm.onsale = [
+        // Type => 0: Keylogger, 1: Analyzer
+        // ------------ Keyloggers ------------
+        {
+          name: 'TT130',
+          type: 0,
+          price: 100,
+          risk: 0.2,
+          hitfactor: 0.1,
+          expiryTime: 0
+        },
+
+        {
+          name: 'XH-5',
+          type: 0,
+          price: 150,
+          risk: 0.1,
+          hitfactor: 0.1,
+          expiryTime: 0
+        },
+
+        {
+          name: 'TT230',
+          type: 0,
+          price: 200,
+          risk: 0.4,
+          hitfactor: 0.2,
+          expiryTime: 15
+        },
+
+        {
+          name: 'XH-7',
+          type: 0,
+          price: 350,
+          risk: 0.4,
+          hitfactor: 0.25,
+          expiryTime: 15
+        },
+
+        {
+          name: 'TT330',
+          type: 0,
+          price: 400,
+          risk: 0.6,
+          hitfactor: 0.4,
+          expiryTime: 30
+        },
+
+        // ------------ Analyzers -------------
+        {
+          name: 'AZ9900',
+          type: 1,
+          price: 100,
+          dataA: 0.4,
+          dataB: 0.1,
+          dataC: 0.01,
+          duration: 6
+        },
+
+        {
+          name: 'AZ8800',
+          type: 1,
+          price: 180,
+          dataA: 0.4,
+          dataB: 0.15,
+          dataC: 0.05,
+          duration: 7
+        },
+
+        {
+          name: 'TM2230',
+          type: 1,
+          price: 250,
+          dataA: 0.5,
+          dataB: 0.2,
+          dataC: 0.05,
+          duration: 5
+        },
+
+        {
+          name: 'SS8000',
+          type: 1,
+          price: 600,
+          dataA: 0.7,
+          dataB: 0.35,
+          dataC: 0.15,
+          duration: 7
+        },
+
+        {
+          name: 'SS9999',
+          type: 1,
+          price: 1500,
+          dataA: 0.85,
+          dataB: 0.4,
+          dataC: 0.2,
+          duration: 8
+        }
+      ];
+
+      this.updatetime = function () {
+        return (gametime = setInterval(1000));
+      }.();
 
 
       // Public functions
       this.initGame = function () {
-        this.cash = 0;
-        this.inventory = {};
-        blackm.onsale.keylogger.push(Begin);
+        player.cash = 200;
+        player.inventory = {};
       };
 
       this.newGame = function () {
         this.initGame();
       };
+
+      this.getPlayer = function () {
+        return player;
+      };
+
+      this.getBlackM = function () {
+        return blackm;
+      }
+
+      this.buy = function (a){   //buy from BlackM
+        if (player.cash >= blackm.onsale[a].price) {
+          player.cash -= blackm.onsale[a].price;
+          player.inventory.push(blackm);
+          cashCallback(player.cash);
+          //blackm.onsale.delete(a);
+          return;
+        } else {
+          alert("You do not have enough money!");
+          return;
+        }
+      }
+
+      this.sell = function(a) {
+        function sell(){  // sell to BlackM
+          return (
+              blackm.sellprice.unencrypteddata * player.produce.unencrypteddata +
+              blackm.sellprice.credentials * player.produce.credentials +
+              blackm.sellprice.creditcard * player.produce.creditcard +
+              blackm.sellprice.identity * player.produce.identity
+              );
+        }
+      }
 
     });
