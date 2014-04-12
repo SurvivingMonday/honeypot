84d74577aac20409dad2e6e4a8e19d79a7e04412
girishvk <girishvk@umich.edu>
2014-04-11 21:49:15 -0400

    inventory

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.prototype = function () {
     var player = {};
     player.cash = 0;
     player.inventory = [];
-	alert(player.cash);
+	
 	// 3 Types of Keylogger( Beginner, Medium, Hard)
 	// hitfactor and risk to be generate depending on type.
 	// Keylogger Begin
@@ -15,42 +15,42 @@ app.prototype = function () {
 	Begin.risk = 0.2; 
 	Begin.hitfactor = 0.4;
 	
-	var blackm = {};// arrays to retain everything
+	// Keylogger Medium
+	var Medium ={};
+	Medium.price = 200;
+	Medium.risk = 0.4;
+	Medium.hitfactor = 0.6;
+	
+	// Keylogger Hard
+	var Hard ={};
+	Hard.price = 400;
+	Hard.risk = 0.6;
+	Hard.hitfactor = 0.8;
+	
+	//BlackM
+	var blackm = {};
 	blackm.credentials=[];
 	blackm.creditcard=[];
 	blackm.unencrypteddata=[];
 	blackm.identity=[];
 	blackm.keylogger=[];
-	for
 	blackm.keylogger.push("keyloggerA": Begin);
-	
+	blackm.keylogger.push("keyloggerB": Medium);
+	blackm.keylogger.push("keyloggerC": Hard);
 
-	// Keylogger Medium
-	keylogger.Medium ={};
-	keylogger.Medium.price = 200;
-	keylogger.Medium.risk = 0.4;
-	keylogger.Medium.hitfactor = 0.6;
-	
-	// Keylogger Hard
-	keylogger.Hard ={};
-	keylogger.Hard.price = 400;
-	keylogger.Hard.risk = 0.6;
-	keylogger.Hard.hitfactor = 0.8;
-
-	var asset ={}; // Assets Uncrypted data(credentials,uncrypted data)	
-	asset.uncrypteddata=0;
-	asset.credentials=0;
+	//var asset ={}; // Assets Uncrypted data(credentials,uncrypted data)	
+	//asset.uncrypteddata=0;
+	//asset.credentials=0;
 		
  // Private functions
- 
 
     return {
         // Public functions
         getCash: function () {    //display cash
             return player.cash;    
         },
-		buy: function(keylogger){   // buy from
-		//checking if keylogger
+		getinventory: function(){   // buy from
+		
 			player.cash-=player.inventory.price;
 			return  
 		}
