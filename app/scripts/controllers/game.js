911eddc5a4e321bc24f1fa36a2eb3ef5dab7d258
Sofwan Haddad <sysofwan@umich.edu>
2014-04-12 09:24:50 -0400

    added feature to select inventory

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -2,10 +2,19 @@
 
 angular.module('honeypotApp')
     .controller('GameCtrl', function ($scope, GameManager, GameMapService) {
+    	var selectedIdx = null;
     	$scope.player = GameManager.getPlayer();
     	$scope.blackMarket = GameManager.getBlackM();
     	console.log($scope.player.inventory);
 
     	$scope.notifyBuy = GameManager.buy;
     	$scope.notifySell = GameManager.sell;
+    	
+    	$scope.selectInv = function(idx) {
+    		if (selectedIdx !== null) {
+    			$scope.player.inventory[selectedIdx].selected = false;
+    		}
+    		selectedIdx = idx;
+    		$scope.player.inventory[idx].selected = true;
+    	};
     });
